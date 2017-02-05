57cee5d5c79f2c3a8c500b09706487a8cafca43b
ivan196 <ivancrespoc@gmail.com>
2017-02-05 14:18:43 +0100

    primera base de datos

---
+++ practicas/tienda.sql
@@ -0,0 +1,114 @@
+drop database if exists tienda_moviles;
+
+create database tienda_moviles;
+
+use tienda_moviles;
+
+create table clientes (
+	DNI char(9),
+	nombre varchar(15),
+	apellidos varchar(20),
+	telefono char(9),
+	email varchar(30),
+	primary key (DNI)
+);
+
+create table tiendas (
+	nombre varchar(15),
+	provincia varchar(15),
+	localidad varchar(15),
+	direccion varchar(30),
+	telefono char(9),
+	diaapertira varchar(10),
+	diacierre varchar(10),
+	horaapertura time,
+	horacierre time,
+	primary key (nombre)
+);
+
+create table operadoras (
+	nombre varchar(20),
+	colorlogo varchar(10),
+	porcentajecobertura varchar(5),
+	frecuenciaGSM varchar(5),
+	paginaweb varchar(50),
+	primary key (nombre)
+);
+
+create table tarifas (
+	nombre varchar(20),
+	nombre_operadoras varchar(20),
+	tamañodatos varchar(20),
+	tipodatos varchar(20),
+	minutosgratis varchar(15),
+	smsgratis varchar(15),
+	primary key (nombre, nombre_operadoras),
+	foreign key (nombre_operadoras)
+	references operadoras (nombre)
+);
+
+create table moviles (
+	marca varchar(20),
+	modelo varchar(20),
+	descripcion varchar(250),
+	SO varchar(15),
+	ram varchar(15),
+	pulgadaspantalla varchar(20),
+	camaraMpx varchar(10),
+	primary key (marca, modelo)
+);
+
+create table movil_libre (
+	marca_moviles varchar(20),
+	modelo_moviles varchar(20),
+	precio varchar(10),
+	primary key (marca_moviles,modelo_moviles),
+	foreign key (marca_moviles,modelo_moviles) references moviles (marca,modelo)
+);
+
+create table movil_contrato (
+	marca_moviles varchar(20),
+	modelo_moviles varchar(20),
+	nombre_operadoras varchar(20),
+	precio varchar(15),
+	primary key (marca_moviles, modelo_moviles,nombre_operadoras),
+	foreign key (marca_moviles,modelo_moviles) references moviles(marca,modelo),
+	foreign key (nombre_operadoras) references operadoras (nombre)
+);
+
+create table ofertas (
+	nombre_operadoras_tarifas varchar(20),
+	nombre_tarifas varchar(20),
+	marca_movil_contrato varchar(20),
+	modelo_movil_contrato varchar(20),
+	primary key (nombre_operadoras_tarifas,nombre_tarifas,marca_movil_contrato,modelo_movil_contrato),
+	foreign key (nombre_operadoras_tarifas,nombre_tarifas) references tarifas (nombre_operadoras,nombre),
+	foreign key (marca_movil_contrato,modelo_movil_contrato) references movil_contrato (marca_moviles,modelo_moviles)
+);
+
+create table compras (
+	DNI_cliente char(9),
+	nombre_tienda varchar(20),
+	marca_moviles_ofertas varchar(20),
+	modelo_moviles_ofertas varchar(20),
+	dia date,
+	primary key (DNI_cliente,nombre_tienda,marca_moviles_ofertas,modelo_moviles_ofertas),
+	foreign key (DNI_cliente) references clientes (DNI),
+	foreign key (nombre_tienda) references tiendas (nombre),
+	foreign key (marca_moviles_ofertas,modelo_moviles_ofertas) references ofertas (marca_movil_contrato,modelo_movil_contrato)
+);
+
+create table contratos (
+	DNI_cliente char(9),
+	nombre_tienda varchar(20),
+	nombre_operadoras_tarifas_ofertas varchar(20),
+	nombre_tarifas_ofertas varchar(20),
+	marca_moviles_ofertas varchar(20),
+	modelo_moviles_ofertas varchar(20),
+	dia date,
+	primary key (DNI_cliente,nombre_tienda,nombre_operadoras_tarifas_ofertas,nombre_tarifas_ofertas,marca_moviles_ofertas,modelo_moviles_ofertas),
+	foreign key (DNI_cliente) references clientes (DNI),
+	foreign key (nombre_tienda) references tiendas (nombre),
+	foreign key (nombre_operadoras_tarifas_ofertas,nombre_tarifas_ofertas) references ofertas (nombre_operadoras_tarifas,nombre_tarifas),
+	foreign key (marca_moviles_ofertas,modelo_moviles_ofertas) references ofertas (marca_movil_contrato,modelo_movil_contrato)
+);
